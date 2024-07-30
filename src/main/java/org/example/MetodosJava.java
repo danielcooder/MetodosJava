7e7485e17017ba2a115d12a709fc269cbce0c3d2
daniel cooder <danielcooder@gmail.com>
2024-07-30 16:36:43 -0300

    feat: Métodos Getter, Setter e Construtor

---
+++ src/main/java/org/example/MetodosJava.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class MetodosJava {
+
+    public static void main(String[] args) {
+
+       Caneta c1 = new Caneta("Deluxe", "Azul", 0.5f);
+        c1.status();
+        Caneta c2 = new Caneta("Compactor", "Vermelha", 1.9f);
+        c2.status();
+        Caneta c3 = new Caneta("Bic", "Preta", 1.5f);
+        c3.status();
+        Caneta c4 = new Caneta("Faber", "Verde", 0.9f);
+        c4.status();
+
+
+
+    }
+}
